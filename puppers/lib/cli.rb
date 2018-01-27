21bc8a8ce9351cb51507ec8fc11bdcc1b338e582
ElleeB <laura.trager@gmail.com>
2018-01-27 12:26:36 -0500

    CLI stub #1

--- puppers/lib/cli.rb
+++ puppers/lib/cli.rb
@@ -11,6 +11,7 @@ class Puppers::CLI
     puts ""
 
     display_shelters
+    display_shelter_details
 
     # gets input
     # display shelters w/name, city, cross streets
@@ -31,28 +32,53 @@ class Puppers::CLI
       puts "3. I Want A Ferret"
     else
       puts "Please enter a valid zip code."
+      display_shelters
     end
   end
 
   def display_shelter_details
-    # asks which shelter in which user is interested
+    # asks which shelter user is interested
+    puts "Enter a number for more details about your shelter, or enter 'search' to go back to search."
     # gets input
+    input = gets.strip
     # display specific shelter details
+    # check if valid - valid_shelter_selection(input)
+    # if valid_shelter_selection?(input), then...
+    if input == "1"
+      puts "Puppies R Us"
+      puts "555 5th Avenue"
+      puts "Ourtown, USA 48070"
+    elsif input == "2"
+      puts "Kitties are The Best"
+      puts "433 Kittylitter Blvd"
+      puts "Sillypaws, USA 48070"
+    elsif input == "3"
+      puts "I Want A Ferret"
+      puts "29067 Fluffy Lane"
+      puts "Sillyface, USA 48070"
+    elsif input == "search"
+      self.call
+    end
     # give option to go back to the list OR exit
-    # if exit goodbye
+    puts "To return to the Shelter List, please enter 'back', or 'exit' to quit"
+    input = gets.strip
+    if input == "back"
+      self.display_shelters #zip variable? ## this does not work, obviously
+    elsif input == "exit"
+      goodbye
+    end
   end
 
   def valid_zip?(zip)
     zip == "48070" ? true : false
   end
 
-  def exit
-    # ask what user would like to do
-    # goodbye if done
+  def valid_shelter_selection?(input)
+    # use regex valid if 1 - highest number of returned shelters
   end
 
   def goodbye
-    "Thank you for considering rescue!"
+    puts "Thank you for considering a rescue companion!"
   end
 
 end
