d17c9e23324373f7982cf961265286f2d4340fa5
ElleeB <laura.trager@gmail.com>
2018-01-28 13:29:33 -0500

    Cont. Scraping - notes added

--- puppers/lib/puppers.rb
+++ puppers/lib/puppers.rb
@@ -3,7 +3,7 @@
 require_relative './puppers/version'
 require_relative './cli.rb'
 require_relative './shelter.rb'
+require_relative './scraper.rb'
 
 require 'nokogiri'
 require 'open-uri'
-require 'pry' #take out for publishing
--- puppers/lib/scraper.rb
+++ puppers/lib/scraper.rb
@@ -1,2 +1,103 @@
 class Scraper
+
+  @shelters_array = []
+
+  def self.scrape_pet_index
+    #this requires the use of the zip code
+    doc = Nokogiri::HTML(open("https://www.petfinder.com/animal-shelters-and-rescues/search/"))
+    puts doc.css(".search-results-container")[0].text
+
+  end
+
 end
+
+
+# puts doc.css(".search-results-container")[0].text
+# =>
+
+# Search For Animal Welfare Groups by Distance:
+#
+# Location: Ex: Atlanta, GA or 30303
+#
+# Group Name
+#
+# Search by state
+#
+# Search For Animal Welfare Groups by State:
+#
+# State
+#
+# Any
+# Alabama
+# Alaska
+# Alberta
+# Arizona
+# Arkansas
+# British Columbia
+# California
+# Colorado
+# Connecticut
+# Delaware
+# District of Columbia
+# Florida
+# Georgia
+# Guam
+# Hawaii
+# Idaho
+# Illinois
+# Indiana
+# Iowa
+# Kansas
+# Kentucky
+# Louisiana
+# Maine
+# Manitoba
+# Maryland
+# Massachusetts
+# Michigan
+# Minnesota
+# Mississippi
+# Missouri
+# Montana
+# Nebraska
+# Nevada
+# New Brunswick
+# New Hampshire
+# New Jersey
+# New Mexico
+# New York
+# Newfoundland
+# North Carolina
+# North Dakota
+# Northwest Territories
+# Nova Scotia
+# Ohio
+# Oklahoma
+# Ontario
+# Oregon
+# Pennsylvania
+# Prince Edward Island
+# Puerto Rico
+# Quebec
+# Rhode Island
+# Saskatchewan
+# South Carolina
+# South Dakota
+# Tennessee
+# Texas
+# Utah
+# Vermont
+# Virginia
+# Washington
+# West Virginia
+# Wisconsin
+# Wyoming
+# Yukon Territory
+# Bahamas
+# Costa Rica
+# Mexico
+# Honduras
+#
+# Group Name
+#
+# Search by distance
